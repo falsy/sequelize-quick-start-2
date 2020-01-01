471c6d955633df76c52661959ed7b13101bd15b6
falsy <me@falsy.me>
2020-01-01 18:39:02 +0900

    add insert API

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ module.exports = () => {
   const models = db.models;
 
   router.get('/list', async (ctx) => {
-
   	const results = await models.boards.findAll().map(({ id, name, content, created_at}) => {
       return { id, name, content, created_at };
     });
@@ -14,8 +13,20 @@ module.exports = () => {
     ctx.body = {
       results
     };
-    
   });
+
+  router.post('/add', async (ctx) => {
+    const { name, content } = ctx.request.body;
+
+    await models.boards.create({
+      name,
+      content
+    }).catch(e => {
+      console.log(e);
+    });
+
+    ctx.status = 204;
+  })
 
   return router;
 };
